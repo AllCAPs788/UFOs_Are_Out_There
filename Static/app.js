e6e705bf2f1d74c628b035407c43e2877e9d5887
ALLCAPs788 <capspfifer@gmail.com>
2020-12-10 05:28:18 -0500

    some examples

--- Static/app.js
+++ Static/app.js
@@ -1,4 +1,20 @@
 // from data.js
-let tableData = data;
+let tableData = data({
+    //example of data pairing from data.js
+    datetime: "1/13/2010",
+    city: "gulf breeze",
+    state: "fl",
+    country: "us",
+    shape: "unknown",
+    durationMinutes: "4 minutes",
+    comments: "Bright orange object&#44 rapid speed and direction changes .."
 
+
+
+
+});
+joinedArray = tableData.join(", ");
 // YOUR CODE HERE!
+
+//create element statement from w3 for HTML DOM
+let x = document.createElement("TH");
