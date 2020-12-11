a1fead0e9ee14bfa08dba2bd38d1bb8ddd5cdc33
ALLCAPs788 <capspfifer@gmail.com>
2020-12-11 16:38:33 -0500

    added loop

--- Static/app.js
+++ Static/app.js
@@ -1,17 +1,53 @@
 
-let tableData = data;
-
+//learn how to import and iterate through data.js table
 /*Use a date form in your HTML document and write JavaScript code that will listen for events 
-    and search through the date/time column to find rows that match user input.*/
+and search through the date/time column to find rows that match user input.*/
+
+let tableData = data;
+function printName(datetime) {
+    console.log(datetime);
+    }
+    for (var i = 0; i < data.length; i++) {
+        printName(datetime[i]);
+
+    } 
+//stackoverflow alternative    
+    let tbodyRef = document.getElementById('ufo-table').getElementsByTagName('tbody')[0];
+    // Insert a row at the end of table
+    let newRow = tbodyRef.insertRow();
+
+    // Insert a cell at the end of the row
+    let newCell = newRow.insertCell();
+
+    // Append a text node to the cell
+    let newText = document.createTextNode('new row');
+    newCell.appendChild(newText);
+    
 
 
-});
+//});
 //joinedArray = tableData.join(", ");
 // YOUR CODE HERE!
 
-//create element statement from w3 for HTML DOM
-//let x = document.createElement("TH");
 
-// function LoadScript() {
-//     let tableData = document.createElement("data.js");
-//     script.src = url;
+//class example
+// Array of student names
+//var students = ["Johnny", "Tyler", "Bodhi", "Pappas"];
+
+// This function will be called for each element in the array
+
+
+
+// Loop through each student name and call the printName function
+
+
+
+// `forEach` automatically iterates (loops) through each item and
+// calls the supplied function for that item.
+// This is equivalent to the for loop above.
+//students.forEach(printName);
+
+// You can also define an anonymous function inline
+//students.forEach(function(name) {
+  //console.log(name);
+//});
