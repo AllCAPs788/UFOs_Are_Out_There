c255b8a9cb3af08c4abe26eb77b37d5d64ac5577
ALLCAPs788 <capspfifer@gmail.com>
2020-12-11 19:22:09 -0500

    tutor help

--- Static/app.js
+++ Static/app.js
@@ -3,7 +3,11 @@
 /*Use a date form in your HTML document and write JavaScript code that will listen for events 
 and search through the date/time column to find rows that match user input.*/
 
+//better example from Day 3
+var tbody = d3.select("tbody");
 let tableData = data;
+
+//only prints 
 function printName(datetime) {
     console.log(datetime);
     }
