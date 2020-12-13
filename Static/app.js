8940ee50971c6d85939544774309dade33ad53e0
ALLCAPs788 <capspfifer@gmail.com>
2020-12-13 17:33:56 -0500

    example

--- Static/app.js
+++ Static/app.js
@@ -12,43 +12,6 @@ console.log(data);
 
 
     } 
-// //stackoverflow alternative    
-//     let tbodyRef = document.getElementById('ufo-table').getElementsByTagName('tbody')[0];
-//     // Insert a row at the end of table
-//     let newRow = tbodyRef.insertRow();
-
-//     // Insert a cell at the end of the row
-//     let newCell = newRow.insertCell();
-
-//     // Append a text node to the cell
-//     let newText = document.createTextNode('new row');
-//     newCell.appendChild(newText);
     
+// refer to Day 3, solved for exercise 9
 
-
-//});
-//joinedArray = tableData.join(", ");
-// YOUR CODE HERE!
-
-
-//class example
-// Array of student names
-//var students = ["Johnny", "Tyler", "Bodhi", "Pappas"];
-
-// This function will be called for each element in the array
-
-
-
-// Loop through each student name and call the printName function
-
-
-
-// `forEach` automatically iterates (loops) through each item and
-// calls the supplied function for that item.
-// This is equivalent to the for loop above.
-//students.forEach(printName);
-
-// You can also define an anonymous function inline
-//students.forEach(function(name) {
-  //console.log(name);
-//});
