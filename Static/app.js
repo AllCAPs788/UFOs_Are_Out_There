84e1067141b79b443d07d0cb3382945f1bfd22e6
ALLCAPs788 <capspfifer@gmail.com>
2020-12-14 21:56:09 -0500

    moved .vscode

--- Static/app.js
+++ Static/app.js
@@ -1,14 +1,19 @@
 
 //learn how to import and iterate through data.js table
-/*Use a date form in your HTML document and write JavaScript code that will listen for events 
-and search through the date/time column to find rows that match user input.*/
+/*Use a date form in your HTML document 
+and write JavaScript code that will listen for events 
+and search through the date/time column to find rows 
+that match user input.*/
 
 //better example from Day 3
-var tbody = d3.select("tbody");
+let tbody = d3.select("tbody");
 let tableData = data;
 
 console.log(data);
 //only prints 
+
+// Clear the value from the text box
+d3.event.target.value = "";
 
 
     } 
