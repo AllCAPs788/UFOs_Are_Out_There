14112cf07d77f7c7d63e11b78ed291d4eb405135
ALLCAPs788 <capspfifer@gmail.com>
2020-12-16 22:29:29 -0500

    new table

--- Static/app.js
+++ Static/app.js
@@ -4,46 +4,67 @@
 and write JavaScript code that will listen for events 
 and search through the date/time column to find rows 
 that match user input.*/
-let tableData = data;
+//let tableData = data;
 //better example from Day 3
+
+
+//1. create table template
 let tbody = d3.select("tbody");
+data.forEach(obj => {
+  let row = tbody.append("tr");
+  Object.entries(obj).forEach(([key, value]) => {
+    let cell = row.append("td");
+    cell.text(value);
+  });
+});
 
-//let date_filter = date_filter.map(person => person.age);
-
-let button = d3.select("#filter-btn");
-
-// Select the form
-let form = d3.select("#form-control");
-
-// Create event handlers for clicking the button or pressing the enter key
-button.on("click", runEnter);
-form.on("submit",runEnter);
-
-// Create the function to run for both events
-function runEnter() {
-    var inputElement = d3.select("#datetime");
-
-    // Get the value property of the input element
-    var inputValue = inputElement.property("value");
-
-}
+//2. get datetime for each object
+let date_filter = data.map(obj => obj.datetime);
+console.log(date_filter)
+//repeat above line for each column name in data.js
 
 
 
-  // Prevent the page from refreshing
-  //d3.event.preventDefault();
 
-  // Select the input element and get the raw HTML node
+
+// let button = d3.select("#filter-btn");
+
+//3. // Select the form
+let date_form = d3.select("#datetime");
+
+//4. listen for events and search through the date/time
+//activities 4-5 on Day 3
+
+// repeat for each column
+// // Create event handlers for clicking the button or pressing the enter key
+// button.on("click", runEnter);
+// form.on("submit",runEnter);
+
+// // Create the function to run for both events
+// function runEnter() {
+//     var inputElement = d3.select("#datetime");
+
+//     // Get the value property of the input element
+//     var inputValue = inputElement.property("value");
+
+// }
+
+
+
+//   // Prevent the page from refreshing
+//   //d3.event.preventDefault();
+
+//   // Select the input element and get the raw HTML node
  
 
-  // Print the value to the console
-  console.log(runEnter);
+//   // Print the value to the console
+//   console.log(runEnter);
 
-// Clear the value from the text box
-//d3.event.target.value = "";
+// // Clear the value from the text box
+// //d3.event.target.value = "";
 
 
-    } 
+//     } 
     
-// refer to Day 3, solved for exercise 9
+// // refer to Day 3, solved for exercise 9
 
