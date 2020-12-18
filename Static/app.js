99e35cf21e77d3e484ea83c765ff9766b0d5efe1
ALLCAPs788 <capspfifer@gmail.com>
2020-12-17 19:41:10 -0500

    late Thurs

--- Static/app.js
+++ Static/app.js
@@ -22,7 +22,7 @@ data.forEach(obj => {
 let date_filter = data.map(obj => obj.datetime);
 console.log(date_filter)
 //repeat above line for each column name in data.js
-let date_filter = data.map(obj => obj.datetime);
+
 let city_filter = data.map(obj => obj.city);
 let state_filter = data.map(obj => obj.state);
 let shape_filter = data.map(obj => obj.shape);
@@ -32,19 +32,25 @@ let comment_filter = data.map(obj => obj.comments);
 
 //3. // Select the form
 let date_form = d3.select("#datetime");
-let city_form = d3.select("#city");
-let state_form = d3.select("#state");
-let shape_form = d3.select("#datetime");
-let duration_form = d3.select("#datetime");
-let comment_form = d3.select("#datetime");
+// let city_form = d3.select("#city");
+// let state_form = d3.select("#state");
+// let shape_form = d3.select("#datetime");
+// let duration_form = d3.select("#datetime");
+// let comment_form = d3.select("#datetime");
 
 //4. listen for events and search through the date/time
 //activities 4-5 on Day 3
 // Input fields can trigger a change event when new text is entered.
-inputField.on("change", function() {
+date_form.on("change", function() {
   var newText = d3.event.target.value;
-  console.log(newText);
+  console.log(date_form);
+// });
+let button = d3.select("#filter-btn");
+button.on("click", function() {
+  d3.select(button);
 });
+
+
 // repeat for each column
 // // Create event handlers for clicking the button or pressing the enter key
 
